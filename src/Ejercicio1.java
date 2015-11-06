287c47bd24c4e15e1bb9fde4b3df467a0d2075ab
Dardo Nogueira <dsanto-tomas@hotmail.com>
2015-11-06 19:27:20 +0100

    finalizar del método PintaPiramide
    finalizado del método que pinta una piramide

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -6,9 +6,26 @@ public class Ejercicio1 extends GraphicsProgram{
 
 
 	public void run(){
-		GLabel etiqueta = new GLabel("HOLA GIT");
-		etiqueta.setLocation(50, 80);
-		add (etiqueta);
+		//cambio el tamaño de la ventana
+		setSize(800,600);
+		//llamo al metodo que dibuja la piramide
+		pintaPiramide();
+	}
+
+	public void pintaPiramide(){
+		int numLadrillosBase = 14;
+		int anchoLadrillo = 30;
+		int altoLadrillo = 12;
+		
+		//querido yo del martes
+		//solo te queda lo mas facil darle la vuelta
+		//Att tu yo del viernes
+		for (int j=0; j< numLadrillosBase; j++){
+			for (int i=0; i< numLadrillosBase-j; i++){
+				GRect ladrillo = new GRect (anchoLadrillo, altoLadrillo);
+				add(ladrillo, anchoLadrillo*j/2 + anchoLadrillo * i, altoLadrillo * j);
+			}
+		}
 	}
 
 }
